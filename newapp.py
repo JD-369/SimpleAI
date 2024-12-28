e01c1a435954551dc73dd5625179befbb33b78fe
JD-369 <mesreehari23@gmail.com>
2024-12-28 13:24:36 +0530

    Update newapp.py

--- newapp.py
+++ newapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import asyncio
 import os
 from together import AsyncTogether, Together
-
+port = os.environ.get("PORT", 8501)
 # Set page configuration
 st.set_page_config(page_title="SimpleAI", page_icon="🤖", layout="wide")
 
